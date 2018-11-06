e639ca10320115ebf7f22c7b054596f493e7d22e
AshmitaR <rykutush@gmail.com>
2018-11-06 19:10:31 +0600

    Update AttendenceSystem.py

--- AttendenceSystem.py
+++ AttendenceSystem.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,request
-import MySQLdb,csv
+from flask import Flask,render_template,request,redirect
+import MySQLdb,csv,urllib2
 import tablib
 import os
 
@@ -12,12 +12,12 @@ dataset = tablib.Dataset()
 
 mydb = MySQLdb.connect(host="localhost", user="", passwd="")
 
-cursor_em = mydb.cursor()
+cursor = mydb.cursor()
 DATABASE_NAME = "employeeattendance"
-cursor_em.execute("SHOW DATABASES")
+cursor.execute("SHOW DATABASES")
 
 check = False
-for x in cursor_em:
+for x in cursor:
     print(x)
     if x == (DATABASE_NAME,):
         check = True
@@ -25,15 +25,13 @@ for x in cursor_em:
         cursor=mydb.cursor()
         break
 if not check:
-    cursor_em.execute("CREATE DATABASE " + DATABASE_NAME)
+    cursor.execute("CREATE DATABASE " + DATABASE_NAME)
     mydb = MySQLdb.connect(host="localhost", user="", db=DATABASE_NAME)
-    cursor_em=mydb.cursor()
-    cursor_em.execute("CREATE TABLE tbl_attendance (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), attendance VARCHAR(255))")
+    cursor=mydb.cursor()
+    cursor.execute("CREATE TABLE tbl_attendance (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), attendance VARCHAR(255))")
+    cursor.execute("CREATE TABLE admin (username VARCHAR(255) PRIMARY KEY, password VARCHAR(255)")
+    cursor.execute("CREATE TABLE employee (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255)")
 
-
-
-conn= MySQLdb.connect(host="localhost", user="root", db="attendence_system")
-cursor = conn.cursor()
 
 @app.route('/',methods=["GET","POST"])
 def index():
@@ -41,10 +39,10 @@ def index():
         username = str(request.form['username'])
         password = str(request.form['password'])
         print(username, password)
-        cursor = conn.cursor()
+        cursor = mydb.cursor()
         cursor.execute('INSERT INTO admin (username,password)VALUES(%s,%s)', (username, password))
         cursor.close()
-        conn.commit()
+        mydb.commit()
     return render_template("index.html")
 
 @app.route('/login')
@@ -61,11 +59,11 @@ def enter_employee():
     if request.method=="POST":
         username = str(request.form['username'])
         password = str(request.form['password'])
-        cursor = conn.cursor()
-        cursor.execute('SELECT username, password FROM admin WHERE username=%s',username)
+        cursor = mydb.cursor()
+        cursor.execute('SELECT username, password FROM admin WHERE username=%s',[username])
         user=cursor.fetchone()
         cursor.close()
-        conn.commit()
+        mydb.commit()
         if user[1]==password:
             return render_template("enter_employee.html")
         else: return "failed"
@@ -74,27 +72,27 @@ def enter_employee():
 def create():
     employee_name = str(request.args.get('employee_name', ""))
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO employee (employee_name)VALUES(%s)', (employee_name))
-    conn.commit()
+    cursor.execute('INSERT INTO employee (employee_name)VALUES(%s)', [employee_name])
+    mydb.commit()
     cursor.close()
     return render_template("enter_employee.html")
 
 @app.route('/employee/export', methods=["GET", "POST"])
 def export():
-    cursor = conn.cursor()
+    cursor = mydb.cursor()
     cursor.execute('SELECT * FROM employee')
     res = cursor.fetchall()
     print res
-    with open('E:\output.csv', 'w') as fileout:
+    with open('output.csv', 'w') as fileout:
         writer = csv.writer(fileout)
         writer.writerows(res)
-    conn.commit()
+    mydb.commit()
     cursor.close()
-    return render_template("enter_employee.html")
-
+    
+    return redirect('http://3.16.75.43/Attendance/output.csv')
 
 @app.route('/employee/show_attendence', methods=["GET", "POST"])
-def show_attendance():
+def show_attendence():
     if request.method == 'POST':
         target = os.path.join(ROOT, 'temp/')
 
@@ -102,7 +100,7 @@ def show_attendance():
             os.mkdir(target)
         file = request.files['file']
         filename = file.filename
-        destination = target + 'temp.csv'
+        destination ='/var/www/html/Attendance/temp/temp.csv'
         file.save(destination)
         with open(destination, 'r') as f:
             dataset.csv = f.read()
@@ -114,7 +112,7 @@ def show_attendance():
             cursor.execute(sql, val)
             mydb.commit()
         return render_template('table.html', dataset=dataset)
- return render_template('index_table.html')
+    return render_template('upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
